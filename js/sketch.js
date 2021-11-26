0096bb33e6a3993d43a58e3428d6fae66995548f
Smithy727 <leanness727@hotmail.com>
2021-11-26 21:35:10 +0000

    added stroke +- and RGB

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,58 @@
+//Global variables 
+var brushSize, brushShape, paintColour;
+var coloursArray = ['red','green','blue'];
+
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(800, 450);
     canvas.parent("p5container");
+    brushSize = 10;
+    background(220);
+
+    paintColour = coloursArray[0]
+
+    console.log(coloursArray)
+
 }
 
 function draw() {
-    background(220);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    if(paintColour === 'red'){
+        fill('rgb(255,0,0)');
+    }else if (paintColour === 'green'){
+        fill('rgb(0,255,0)');
+    }else if (paintColour === 'blue'){
+        fill ('rgb(0,0,255)')
+    }
+    
+    
+    noStroke();
+
+
+    if (mouseIsPressed == true){
+        ellipse(mouseX, mouseY, brushSize);
+    };
+
+
+   
+
+}
+
+function keyPressed(){
+    if(keyCode === UP_ARROW && brushSize <= 50){
+       brushSize = brushSize + 5;
+        console.log(brushSize)
+    } else if (keyCode === DOWN_ARROW && brushSize >= 10) {
+        brushSize= brushSize - 5;
+    }
+
+    if (keyCode === 81){
+        paintColour = coloursArray[0]
+    }
+    if (keyCode === 87){
+        paintColour = coloursArray[1]
+    }
+    
+    if (keyCode === 69){
+    paintColour = coloursArray[2]
+}
 }
